a320cc132f31d1593ffe780c08619cfb18dfeff2
itKebab <56410133+itKebab@users.noreply.github.com>
2021-02-10 20:54:53 +0300

    update

---
+++ Assets/Scripts/pause_controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pause_controller : MonoBehaviour
+{
+   private void OnEnable()
+   {
+      Time.timeScale = 0;
+   }
+
+   private void OnDisable()
+   {
+      Time.timeScale = 1;
+   }
+}
---
+++ Assets/main_menu.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class main_menu : MonoBehaviour
+{
+  private void Start()
+  {
+    Time.timeScale = 0;
+  }
+
+  public void disable_menu()
+  {
+    Time.timeScale = 1;
+    gameObject.SetActive(false);
+  }
+}
